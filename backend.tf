cf8535a9dfbd9175ddafff0fc67b31a25546db82
Yetunde122 <125519830+Yetunde122@users.noreply.github.com>
2023-03-30 13:34:18 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sigmatek-terraform-state"
+    bucket = "sigmatek-terraform-state-yt"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "terraform-state-lock"
